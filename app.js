44d8db9d3beb440ecfbcf5b2974ce739b354d569
tallam-git <tallamcharanteja99@gmail.com>
2023-04-03 20:00:27 -0500

    Added bonus files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var electronicsRouter = require('./routes/electronics');
 var selectorRouter = require('./routes/selector');
+var boardRouter = require('./routes/board');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/electronics',electronicsRouter);
 app.use('/selector',selectorRouter);
+app.use('/board',boardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
