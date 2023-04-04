f891823d550e94f202ddf6d4b6a39b1da6bc8c87
tallam-git <tallamcharanteja99@gmail.com>
2023-04-03 19:36:57 -0500

    Added images and selector files

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var electronicsRouter = require('./routes/electronics');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/electronics',electronicsRouter);
+app.use('/selector',selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
